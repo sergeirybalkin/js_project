2c1d643936aa4c292619ea781b291d8ccb5032b5
Sergei <e1720@yahoo.com>
2020-12-19 22:28:31 +0300

    Added an object

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
-"use strict"
-const user = "Sergei";
-const message = console.log(`Hello ${user}`);
+let numberOfFilms = +prompt("how many movies did you watch?"); //1
+
+let personalMovieDb = {
+    count : +`${numberOfFilms}`,
+    movies : {},
+    actors : {},
+    genres: [],
+    privat : false
+}; //2
+console.log(personalMovieDb.count);
+
+
+let question_first = prompt("Your last movie is");
+let question_second = prompt("How many stars?");
+let answ1 = prompt("Your last movie is");
+let answ2 = prompt("How many stars?");
+
+
+
+personalMovieDb.movies [question_first] = question_second;
+personalMovieDb.movies [answ1] = answ2;
+
+console.log(personalMovieDb);
